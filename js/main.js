b25aaba213e2ee48e9fe84a00ab5e92d7975fa89
inouejiro <72692302+inouejiro@users.noreply.github.com>
2020-10-11 18:58:32 +0700

    add new project

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+
+
+
+// back to top
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 200) {
+    toTop.classList.add("active");
+  } 
+  else {
+    toTop.classList.remove("active");
+  }
+}) 
